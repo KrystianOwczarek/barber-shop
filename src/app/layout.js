e5fcd0ce155d17a2820c5d84e024e417ff6fbf2d
Krystian Owczarek <krystianowczarek@laptop-Krystian.local>
2024-07-23 18:07:53 +0200

    add favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,13 +28,19 @@ export default function RootLayout({ children }) {
           },
           // Additional structured data properties...
         })}</script>
-        <link rel="icon" type="image/png" href="/logo.png" sizes="any"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff"/>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Barber Shop | Raszków</title>
-        <meta name="description" content="Strona barbera z najlepszymi usługami fryzjerskimi i kosmetycznymi. Odkryj profesjonalizm i styl. Terminy online. Obejrzyj nasze projekty już dziś!"/>
+        <meta name="description" content="Strona barbera z najlepszymi usługami fryzjerskimi i kosmetycznymi. Odkryj profesjonalizm i styl. Obejrzyj nasze projekty już dziś!"/>
         <meta property="og:title" content="Barber Shop | Raszków"/>
-        <meta property="og:description" content="Strona barbera z najlepszymi usługami fryzjerskimi i kosmetycznymi. Odkryj profesjonalizm i styl. Terminy online. Obejrzyj nasze projekty już dziś!" />
+        <meta property="og:description" content="Strona barbera z najlepszymi usługami fryzjerskimi i kosmetycznymi. Odkryj profesjonalizm i styl. Obejrzyj nasze projekty już dziś!" />
         {/* <!-- Google tag (gtag.js) --> */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-LHMCFL9JZ4"></script>
       </Head>
