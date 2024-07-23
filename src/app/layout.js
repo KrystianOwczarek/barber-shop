e8215f564e547227de9f9d4321762d61824106a3
Krystian Owczarek <krystianowczarek@laptop-Krystian.local>
2024-07-23 19:14:26 +0200

    add image to open graph

--- src/app/layout.js
+++ src/app/layout.js
@@ -41,15 +41,15 @@ export default function RootLayout({ children }) {
         <meta name="description" content="Strona barbera z najlepszymi usługami fryzjerskimi i kosmetycznymi. Odkryj profesjonalizm i styl. Obejrzyj nasze projekty już dziś!"/>
         <meta property="og:title" content="Barber Shop | Raszków"/>
         <meta property="og:description" content="Strona barbera z najlepszymi usługami fryzjerskimi i kosmetycznymi. Odkryj profesjonalizm i styl. Obejrzyj nasze projekty już dziś!" />
-        <meta property="og:image" content="https://iili.io/dobCnAG.png" />
-        <meta property="og:image:secure_url" content="https://iili.io/dobCnAG.png" />
+        <meta property="og:image" content="https://barbershopraszkow.pl/img/opengraph.png" />
+        <meta property="og:image:secure_url" content="https://barbershopraszkow.pl/img/opengraph.png" />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:width" content="1200" />
         <meta property="og:height" content="640" />
         <meta property="og:image:alt" content="barber shop banner" />
         <meta name="twitter:title" content="Barber Shop | Raszków" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="https://iili.io/dobCnAG.png" />
+        <meta name="twitter:image" content="https://barbershopraszkow.pl/img/opengraph.png" />
         <meta name="twitter:description" content="Strona barbera z najlepszymi usługami fryzjerskimi i kosmetycznymi. Odkryj profesjonalizm i styl. Obejrzyj nasze projekty już dziś!" />
         <meta property="og:url" content="https://barbershopraszkow.pl"/>
         <meta property="og:type" content="website"/>
