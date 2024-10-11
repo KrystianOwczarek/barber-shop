6aa542b6a5a7328fa7e72166471dace33155e6ff
Krystian Owczarek <krystian.owczarek@onet.pl>
2024-10-11 14:36:24 +0200

    scroll mt

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
       <Navigation />
       <section
         id="hero"
-        className="max-w-full w-full flex flex-col-reverse justify-start sm:flex-row p-6 items-center gap-8 mt-20 sm:mt-36 scroll-mt-40"
+        className="max-w-full w-full flex flex-col-reverse justify-start sm:flex-row p-6 items-center gap-8 mt-20 sm:mt-36 scroll-mt-80"
       >
         <article className="max-w-7xl w-full m-auto pl-4">
           <h1 className="max-w-full font-medium mb-5 sm:mb-10 text-start sm:text-5xl sm:text-left barber-color">
